6a3273db0bb2a2842c2613a296451e74bdf38809
VikashAnandJha <mailtovikashjha@gmail.com>
2022-02-02 14:13:19 +0530

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+const express= require('express')
+const app = express()
+const web3 = require('@solana/web3.js');
+const {Keypair} = require("@solana/web3.js")
+
+  
+let connection,bal;
+
+app.get('/',(req, res) => {
+
+    res.send("Hellow BROLLET!")
+    console.log(solanaWeb3);
+}) 
+
+
+
+async function createAddress() {
+    
+    
+   
+ }
+ async function getBalance(address) {
+    let key = new web3.PublicKey(address);
+
+    connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'), 'confirmed');
+      bal = await connection.getBalance(key);
+      bal=bal/web3.LAMPORTS_PER_SOL;
+    console.log(bal)
+  //return bal;
+  //res.send("Balance of this solana account:"+req.params.id+" is "+bal+"\nFilter by:"+req.query.showIn);
+  //res.send("Balance of this solana account:"+bal);
+    
+   
+ }
+
+
+app.get('/api/create',(req, res) => {
+
+    res.send("Creating SOLANA Key")
+
+    let account = Keypair.generate();
+
+console.log(account.publicKey.toBase58());
+console.log(account.secretKey);
+
+})
+app.get('/api/balance/:id',(req, res) => {
+
+   
+    getBalance(req.params.id).then(()=>{
+        res.send("Balance is:"+bal+"SOL")
+    }).catch((err)=>{
+       res.send("Error")
+    })
+
+    
+   
+})
+
+// PORT 
+const PORT= process.env.PORT || 5000
+app.listen(PORT,() => console.log("Listening "+PORT))
+
+
+
+ 
