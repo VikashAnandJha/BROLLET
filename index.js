a83cbd9bab52b5d673052690775ae11367c9e156
VikashAnandJha <mailtovikashjha@gmail.com>
2022-02-08 03:02:03 +0530

    node rendering

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express= require('express')
 const app = express()
-const web3 = require('@solana/web3.js');
-const {Keypair,PublicKey} = require("@solana/web3.js")
-const { AccountLayout, u64,Token,TOKEN_PROGRAM_ID,splToken } = require("@solana/spl-token");
-const { derivePath, getMasterKeyFromSeed, getPublicKey } = require('ed25519-hd-key')
 
-
-
-const bip39 = require("bip39")
-const bip44 = require("bip44")
 var cors = require('cors')
 
 
@@ -18,17 +10,16 @@ var cors = require('cors')
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) //For JSON requests
+app.use(express.static(__dirname+'/public'));
 
 
   
 
-let connection,bal,tokens,enpoint="devnet";
 
 const keyRoute=require('./routes/Key');
 const balanceRoute=require('./routes/Balance');
 
-connection = new web3.Connection(web3.clusterApiUrl(enpoint), 'confirmed');
- 
+
 // PORT 
 const PORT= process.env.PORT || 5000
 app.listen(PORT,() => console.log("Listening "+PORT))
@@ -38,6 +29,8 @@ app.use("/api/",balanceRoute)
 
 app.get('/',(req, res) => {
 
-  res.send("Hellow BROLLET!")
+  res.sendFile('index.html', { root: __dirname });
+
+
  // console.log(solanaWeb3);
 }) 
